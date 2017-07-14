c7eb11334226c08b8cd208c9c88a478d3f0f8cd1
dante0shy <dante0shy@yahoo.com>
2017-07-14 15:59:13 +0800

    minst_refact

--- MNIST/train_refactor.py
+++ MNIST/train_refactor.py
@@ -9,16 +9,37 @@ learning_rate = 0.001
 learning_rate_decay = 0.99
 regularization_rate = 0.0001
 moving_decay = 0.99
-training_step = 3000
+training_step = 5000
+
+def get_weight_variable(shape, reqularizer):
+    weights =  tf.get_variable(
+        "weights",shape,initializer= tf.truncated_normal_initializer(stddev=0.1)
+    )
+
+    if(reqularizer!=None):
+        tf.add_to_collection("losses", reqularizer(weights))
+    return weights
 
 
-def inference(inputT,avg_calss,w1,b1,w2,b2):
-    if avg_calss==None:
-        layers=tf.nn.relu(tf.matmul(inputT,w1)+b1)
-        return tf.matmul(layers,w2)+b2
-    else:
-        layers = tf.nn.relu(tf.matmul(inputT, avg_calss.average(w1)) + avg_calss.average(b1))
-        return tf.matmul(layers,avg_calss.average(w2)) + avg_calss.average(b2)
+def inference(in_tensor,in_shape,out_shape,regularizer):
+    print in_shape
+    with tf.variable_scope('layer1'):
+        weights = get_weight_variable(
+            [in_shape,layers_node],regularizer
+        )
+        biasses= tf.get_variable("biases",
+                                 [layers_node],initializer=tf.constant_initializer(0.0)
+                                 )
+        layer1= tf.nn.relu(tf.matmul(in_tensor,weights)+biasses)
+    with tf.variable_scope('layer2'):
+        weights = get_weight_variable(
+            [layers_node,out_shape],regularizer
+        )
+        biasses = tf.get_variable("biases",
+                                  [out_shape], initializer=tf.constant_initializer(0.0)
+                                  )
+        layer2 = tf.matmul(layer1, weights) + biasses
+    return layer2
 
 def build_model(mnist,in_shape,out_shape):
 
@@ -26,14 +47,9 @@ def build_model(mnist,in_shape,out_shape):
     x = tf.placeholder(tf.float32, shape=(None, in_shape), name="x_input")
     y_ = tf.placeholder(tf.float32, shape=(None, out_shape), name="y_input")
 
-    w1 = tf.Variable(tf.truncated_normal([in_shape,layers_node],stddev=0.1))
-    b1 = tf.Variable(tf.constant(0.1,shape=[layers_node]))
+    regularizer = tf.contrib.layers.l2_regularizer(regularization_rate)
 
-
-    w2 = tf.Variable(tf.truncated_normal([layers_node,out_shape],stddev=0.1))
-    b2 = tf.Variable(tf.constant(0.1, shape=[out_shape]))
-
-    y = inference(x,None,w1,b1,w2,b2)
+    y = inference(x,in_shape,out_shape,regularizer)
 
     global_step = tf.Variable(0, trainable=False)
 
@@ -46,8 +62,6 @@ def build_model(mnist,in_shape,out_shape):
     )
 
     # variable_y=inference(x, None,w1,b1,w2,b2)
-    variable_y=inference(x, variable_averages,w1,b1,w2,b2)
-
 
     cross_entopy = tf.nn.sparse_softmax_cross_entropy_with_logits(
         y, tf.arg_max(y_,1)
@@ -55,16 +69,12 @@ def build_model(mnist,in_shape,out_shape):
 
     cross_entopy_mean= tf.reduce_mean(cross_entopy)
 
-    regularizer = tf.contrib.layers.l2_regularizer(regularization_rate)
-
-    regularization = regularizer(w1)+regularizer(w2)
-
-    loss= cross_entopy+regularization
+    loss= cross_entopy_mean+tf.add_n(tf.get_collection("losses"))
 
     learning_rate_pro = tf.train.exponential_decay(
         learning_rate,
         global_step,
-        minst.train.num_examples,
+        mnist.train.num_examples,
         learning_rate_decay
     )
 
@@ -82,7 +92,7 @@ def build_model(mnist,in_shape,out_shape):
     accuracy = tf.reduce_mean(
         tf.cast(
             tf.equal(
-                tf.arg_max(variable_y,1),
+                tf.arg_max(y,1),
                 tf.arg_max(y_,1)
             ),
             tf.float32
@@ -91,59 +101,34 @@ def build_model(mnist,in_shape,out_shape):
 
     with tf.Session() as sess:
         tf.initialize_all_variables().run()
-        validate_feed = {
-            x : minst.validation.images,
-            y_: minst.validation.labels
-        }
-        test_feed = {
-            x : minst.test.images,
-            y_: minst.test.labels
-        }
-        train_feed = {
-            x : minst.train.images,
-            y_: minst.train.labels
-        }
 
 
         for i in range(training_step):
-            if(i%500==0):
-                loss_m_t = sess.run(
-                    cross_entopy_mean,feed_dict=train_feed
-                )
-                loss_m_v = sess.run(
-                    cross_entopy_mean,feed_dict=validate_feed
-                )
-                accuracy_v = sess.run(
-                    accuracy,feed_dict=validate_feed
-                )
-                print "Round %d, train loss: %g, val loss: %g, val acc: %g" % (i,loss_m_t,loss_m_v,accuracy_v)
-            # print sess.run(w1)
-            # print i
-            x_m,y_m = minst.train.next_batch(batch_size)
-            sess.run(
-                train_op,feed_dict= {
-                    x : x_m,
-                    y_ : y_m
+            x_m, y_m = mnist.train.next_batch(batch_size)
+            _,loss_v,step=sess.run(
+                [train_op,loss,global_step], feed_dict={
+                    x: x_m,
+                    y_: y_m
                 }
             )
+            if(i%500==0):
+                print "Round %d, train loss: %g" % (i,loss_v)
 
-        test_acc = sess.run(
-            accuracy, feed_dict=test_feed
-        )
-
-        print "finial round %d, acc: %g"%(training_step,test_acc)
-        tf.train.Saver().save(sess,"./model/model.ckpt")
+                tf.train.Saver().save(sess,"./model_re/model.ckpt",global_step=global_step)
         # print sess.run(w1)
         # print sess.run(w2)
 
-if __name__=='__main__':
+
+def main(argv=None):
     minst = input_data.read_data_sets("./train", one_hot=True)
-    Input_node=minst.train.images[0].shape[0]
-    Output_node=minst.train.labels[0].shape[0]
+    Input_node = minst.train.images[0].shape[0]
+    Output_node = minst.train.labels[0].shape[0]
 
+    build_model(minst, Input_node, Output_node)
 
-    build_model(minst,Input_node,Output_node)
+if __name__=='__main__':
 
+    tf.app.run()
 
 
 
---
+++ MNIST/train_refactor_eval.py
@@ -0,0 +1,56 @@
+import time
+import tensorflow as tf
+import train_refactor
+from tensorflow.examples.tutorials.mnist import input_data
+
+eval_secs= 10
+
+
+def evaluation(mnist,in_shape,out_shape):
+    with tf.Graph().as_default() as g:
+        x = tf.placeholder(tf.float32, shape=(None, in_shape), name="x_input")
+        y_ = tf.placeholder(tf.float32, shape=(None, out_shape), name="y_input")
+
+        val_feed={
+            x: mnist.validation.images,
+            y_: mnist.validation.labels
+        }
+
+        y=train_refactor.inference(x,in_shape,out_shape,None)
+        corr_pre= tf.equal(tf.arg_max(y,1),tf.arg_max(y_,1))
+        acc=tf.reduce_mean(tf.cast(corr_pre,tf.float32))
+
+        variables_averages= tf.train.ExponentialMovingAverage(
+            train_refactor.moving_decay
+        )
+
+        variables_to_restore= variables_averages.variables_to_restore()
+
+        saver=tf.train.Saver(variables_to_restore)
+        while True:
+            with tf.Session() as sess:
+                ckpt=tf.train.get_checkpoint_state(
+                    "./model_re"
+                )
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess,ckpt.model_checkpoint_path)
+                    global_step= ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+
+                    acc_score=sess.run(acc,feed_dict=val_feed)
+                    print ("Round %s acc : %g"%(global_step,acc_score))
+                else:
+                    print "no ckpt"
+                    return
+                time.sleep(eval_secs)
+
+
+def main(argv=None):
+    minst = input_data.read_data_sets("./train", one_hot=True)
+    Input_node = minst.train.images[0].shape[0]
+    Output_node = minst.train.labels[0].shape[0]
+
+    evaluation(minst, Input_node, Output_node)
+
+if __name__=='__main__':
+
+    tf.app.run()
